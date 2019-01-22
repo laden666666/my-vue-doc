e5438100b65db02bb6fb82f594f03cf85f23b60b
DESKTOP-EKK8J81\njz <njz198869@aliyun.com>
2019-01-22 17:11:42 +0800

    增加文档几个控件

---
+++ src/components/Table/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <table class="table"><slot></slot></table>
+</template>
+<style lang="less" scoped>
+.table{
+    margin : 15px 0px 0px ;
+    padding : 0px ;
+    border : 1px solid @border-color ;
+    border-collapse : collapse ;
+    width : 100% ;
+    color : rgb(0, 0, 0) ;
+    font-size : 14px ;
+    background-color : rgb(253, 252, 248) ;
+    margin-top: -1px;
+    margin-bottom: 8px;
+}
+</style>
